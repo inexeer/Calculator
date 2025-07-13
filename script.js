597e7d3d6f0f7cad33d58cc2d541c294b5fbc4c8
Konstantin <135962360+inexeer@users.noreply.github.com>
2025-07-13 18:53:37 +0000

    to get on mint

---
+++ script.js
@@ -0,0 +1,33 @@
+let num1;
+let num2;
+let op;
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num1){
+    return num1 / num2;
+}
+
+function operate(op, num1, num2){
+    switch(op){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
